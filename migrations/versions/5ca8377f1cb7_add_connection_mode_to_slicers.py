157b094b366293f4aff173dbe621b5f8bffe59d9
Jeff Johnston <jj358mhz@gmail.com>
2026-09-12 22:10:40 -0700

    chore: modernize type annotations in migration to satisfy ruff UP007

--- migrations/versions/5ca8377f1cb7_add_connection_mode_to_slicers.py
+++ migrations/versions/5ca8377f1cb7_add_connection_mode_to_slicers.py
@@ -5,17 +5,16 @@ Revises: b5430b1dd73e
 Create Date: 2026-09-12 20:52:57.900693
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '5ca8377f1cb7'
-down_revision: Union[str, None] = 'b5430b1dd73e'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = 'b5430b1dd73e'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
